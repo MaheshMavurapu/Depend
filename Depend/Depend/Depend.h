fabfa338095f88ce065c80be1beff20ce2117cfd
Mahesh Mavurapu <maheshmavurapu@Maheshs-MacBook-Pro.local>
2018-12-19 20:23:47 +0530

    Initial Commit

---
+++ Depend/Depend/Depend.h
@@ -0,0 +1,19 @@
+//
+//  Depend.h
+//  Depend
+//
+//  Created by Mahesh Mavurapu on 19/12/18.
+//  Copyright © 2018 Mahesh Mavurapu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Depend.
+FOUNDATION_EXPORT double DependVersionNumber;
+
+//! Project version string for Depend.
+FOUNDATION_EXPORT const unsigned char DependVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Depend/PublicHeader.h>
+
+
